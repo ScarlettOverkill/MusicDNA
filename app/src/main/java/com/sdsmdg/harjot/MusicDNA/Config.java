8d3de2a5077baf02929bd0fbb2c0935e6d2f7fd4
Ataberk Olgun <olgunataberk@gmail.com>
2017-02-07 00:41:47 +0300

    added dummy Config.java to build and launch app

---
+++ app/src/main/java/com/sdsmdg/harjot/MusicDNA/Config.java
@@ -0,0 +1,10 @@
+package com.sdsmdg.harjot.MusicDNA;
+
+/**
+ * Created by ataberk on 6.2.2017.
+ */
+
+public class Config {
+    public static final String CLIENT_ID = "YOUR_SOUNDCLOUD_CLIENT_ID";
+    public static final String API_URL = "https://api.soundcloud.com";
+}
